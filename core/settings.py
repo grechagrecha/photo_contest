849fdd446c8d4ba0ef4e5a6dac4cc6622bdc4d08
root <root@desktop-left>
2024-01-29 17:59:04 +0300

    more ui improvements

--- core/settings.py
+++ core/settings.py
@@ -120,8 +120,13 @@ TIME_ZONE = 'Europe/Moscow'
 USE_TZ = True
 USE_I18N = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = 'media/'
 
