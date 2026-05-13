8c81b72f79b5e89eeb5782a761d06468357fb540
grechagrecha <ruspolak69@gmail.com>
2026-05-13 16:31:40 +0300

    feature(users): add api token auto creation with post_save signal

---
+++ apps/users/signals/signals.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+# TODO: Move core/signals to apps/users/signals
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        t = Token.objects.create(user=instance)
+        print(f'post_save signal working, token {t} for user {instance} was created')
+    else:
+        print(f'post_save signal working, user already has a token')
+        
